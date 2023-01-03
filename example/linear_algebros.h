3f21b72b293233d40750896fd376a2c3efbd891a
Andrew Mengede <amengede27@outlook.com>
2023-01-03 22:01:38 +1000

    Update 230103

--- example/linear_algebros.h
+++ example/linear_algebros.h
@@ -1,4 +1,5 @@
 #include <intrin.h>
+#include <stdbool.h>
 
 #ifndef __linear_algebros_h__
 #define __linear_algebros_h__
@@ -19,16 +20,24 @@ typedef struct vec4 {
 
 typedef struct mat4 {
 	union {
+		__m256 chunk[2];
 		__m128 column[4];
 		vec4 column_vector[4];
 		float data[16];
 	};
 } mat4;
 
+typedef struct quaternion {
+	union {
+		__m128 vector;
+		float data[4];
+	};
+} quat;
+
 /*-------- Conversions        ----------*/
 
 #define pi 3.14159265359f
-#define eps 1e-30f
+#define eps 1e-10f
 
 /**
 	Convert from degrees to radians.
@@ -73,6 +82,7 @@ float linalgDotVec3(vec3 a, vec3 b);
 vec3 linalgCross(vec3 a, vec3 b);
 
 /**
+	\param a the vector to normalize, cannot have zero magnitude
 	\returns a new vec3, being parallel with the input a, having unit length.
 */
 vec3 linalgNormalizeVec3(vec3 a);
@@ -107,11 +117,11 @@ vec3 linalgMulVec3(vec3 a, float scalar);
 /**
 	Get the angle between two vectors.
 
-	\param a the first vector
-	\param b the second vector
+	\param a the first vector, cannot have zero magnitude
+	\param b the second vector, cannot have zero magnitude
 	\returns the angle between the vectors a & b, in degrees
 */
-float angleBetweenVectors3(vec3 a, vec3 b);
+float linalgAngleBetweenVectors3(vec3 a, vec3 b);
 
 /**
 	Get the projection of one vector onto another.
@@ -121,10 +131,10 @@ float angleBetweenVectors3(vec3 a, vec3 b);
 			= projection(v onto u) + rejection(v onto u)
 
 	\param incoming the vector to be projected
-	\param basis the vector onto which to be projected
+	\param basis the vector onto which to be projected, cannot have zero magnitude
 	\returns a new vector, parallel with basis, storing the vector projection of incoming onto basis
 */
-vec3 project(vec3 incoming, vec3 basis);
+vec3 linalgProject(vec3 incoming, vec3 basis);
 
 /**
 	Get the rejection of one vector onto another.
@@ -134,10 +144,74 @@ vec3 project(vec3 incoming, vec3 basis);
 			= projection(v onto u) + rejection(v onto u)
 
 	\param incoming the vector to be rejected
-	\param basis the vector to do the rejecting
+	\param basis the vector to do the rejecting, cannot have zero magnitude
 	\returns a new vector, orthogonal to basis, storing the vector rejection of incoming from basis
 */
-vec3 reject(vec3 incoming, vec3 basis);
+vec3 linalgReject(vec3 incoming, vec3 basis);
+
+/**
+	Compute a vector reflection.
+
+	\param incident a direction vector incident to (pointing towards) the point of impact.
+	\param normal the normal vector about which to reflect. Must have unit length.
+	\returns a new vector representing the direction after reflecting.
+*/
+vec3 linalgReflect(vec3 incident, vec3 normal);
+
+/**
+	Linearly interpolate between two vectors.
+
+	\param a the first vector
+	\param b the second vector
+	\param t the interpolation parameter. Typically between 0 and 1, though this isn't enforced
+	\returns a new vector, being a linear interpolation between a and b.
+*/
+vec3 linalgLerpVec3(vec3 a, vec3 b, float t);
+
+/**
+	Spherical Linear interpolation between two vectors.
+	lerp will take a straight line between vectors, on the other hand,
+	slerp interpolates angle-wise, in a rotational sense.
+
+	\param a the first vector, should be normalized.
+	\param b the second vector, should be normalized.
+	\param t the interpolation parameter. Typically between 0 and 1, though this isn't enforced
+	\returns a new vector, being a linear interpolation between a and b.
+*/
+vec3 linalgSlerpVec3(vec3 a, vec3 b, float t);
+
+/**
+	Normalized Linear interpolation between two vectors.
+	Normalizing the result of lerp will approximate slerp.
+
+	\param a the first vector, should be normalized.
+	\param b the second vector, should be normalized.
+	\param t the interpolation parameter. Typically between 0 and 1, though this isn't enforced
+	\returns a new vector, being a linear interpolation between a and b.
+*/
+vec3 linalgNlerpVec3(vec3 a, vec3 b, float t);
+
+/**
+	Indicates whether two vectors are within epsilon of one another.
+*/
+bool linalgCloseVec3(vec3 a, vec3 b);
+
+/*-------- Vector4 Operations ----------*/
+
+/*
+	Under the hood, vec3 is really a vec4 with zeroed out w component,
+	however as convention has it right now, it's being treated as a seperate type.
+*/
+
+/**
+	\returns a normalized copy of the given vector.
+*/
+vec4 linalgNormalizeVec4(vec4 a);
+
+/**
+	\returns the dot product result = a.b
+*/
+float linalgDotVec4(vec4 a, vec4 b);
 
 /*-------- Matrix4 Operations ----------*/
 
@@ -217,5 +291,136 @@ vec4 linalgMulMat4Vec4(mat4 m, vec4 v);
 	\returns a new mat4 representing the matrix multiplication: m3 = m2*m1
 */
 mat4 linalgMulMat4Mat4(mat4 m1, mat4 m2);
+
+/**
+	\returns the matrix sum m3 = m1 + m2
+*/
+mat4 linalgAddMat4(mat4 m1, mat4 m2);
+
+/**
+	\returns the scalar multiplication result = scalar * matrix
+*/
+mat4 linalgMulMat4Scalar(mat4 matrix, float scalar);
+
+/**
+	Blend (linearly interpolate) two matrices.
+
+	\param m1 the start matrix (t = 0)
+	\param m2 the end matrix (t = 1)
+	\param t the interpolation parameter
+	\returns the result m3 = m1 + t * (m2 - m1)
+*/
+mat4 linalgLerpMat4(mat4 m1, mat4 m2, float t);
+
+/**
+	\returns a transposed copy of the given matrix
+*/
+mat4 linalgTranspose(mat4 matrix);
+
+/**
+	Compute a transform matrix inverse.
+
+	General matrix inverses are computationally intense, however
+	most transform matrices can be expressed in the form:
+
+	M = (aX | bY | cZ | I)
+
+	where:
+
+		a: x axis scaling factor
+		X: forwards rotation vector
+		b: y axis scaling factor
+		Y: right rotation vector
+		c: z axis scale factor
+		Z: up rotation vector
+		I: [0 0 0 1]
+
+	Matrices in this form have a closed form inverse.
+
+	Source: https://lxjk.github.io/2017/09/03/Fast-4x4-Matrix-Inverse-with-SSE-SIMD-Explained.html
+
+	\param matrix the matrix to invert
+	\returns the inverse
+*/
+mat4 linalgTransformInverse(mat4 matrix);
+
+/*-------- Quaternion Operations ----------*/
+
+/**
+	\returns a quaternion made from individual components.
+*/
+quat linalgMakeQuaternionFromComponents(float x, float y, float z, float w);
+
+/**
+	Make a quaternion from a rotation operation.
+
+	\param angle the rotation angle (in degrees)
+	\param axis the axis of rotation
+	\returns the corresponding quaternion
+*/
+quat linalgMakeQuaternionFromRotation(float angle, vec3 axis);
+
+/**
+	Make a quaternion tracking a rotation from vector a to vector b.
+*/
+quat linalgMakeRotationFromVec2Vec(vec3 a, vec3 b);
+
+/**
+	\returns the quaternion's axis of rotation
+*/
+vec3 linalgGetAxisFromQuaternion(quat q);
+
+/**
+	\returns the quaternion's angle, in degrees
+*/
+float linalgGetAngleFromQuaternion(quat q);
+
+/**
+	\returns the sum of two quaternions
+*/
+quat linalgAddQuat(quat q1, quat q2);
+
+/**
+	\returns the difference of two quaternions
+*/
+quat linalgSubQuat(quat q1, quat q2);
+
+/**
+	\returns a scaled copy of the quaternion
+*/
+quat linalgMulQuat(quat q, float scalar);
+
+/**
+	\returns the dot product of two quaternions
+*/
+float linalgDotQuat(quat q1, quat q2);
+
+/**
+	\returns whether two quaternions are sufficiently close.
+*/
+bool linalgCloseQuat(quat q1, quat q2);
+
+/**
+	It's possible for two quaternions to be the same, but mathematically different.
+	Ie. a rotation in the opposite angle through the opposite axis is actually still the same.
+
+	\returns whether two quaternions have the same orientation.
+*/
+bool linalgQuatSameOrientation(quat q1, quat q2);
+
+/**
+	\returns a normalized quaternion
+*/
+quat linalgNormalizeQuat(quat q);
+
+/**
+	\returns the conjugate quaternion, a rotation of the same angle, around the opposite axis.
+*/
+quat linalgGetConjQuat(quat q);
+
+/**
+	\returns the inverse of the given quaternion.
+*/
+quat linalgInvQuat(quat q);
 
 #endif // !__linear_algebros_h__
--- linear_algebros.c
+++ linear_algebros.c
@@ -1,4 +1,4 @@
-#include "linear_algebros.h"
+﻿#include "linear_algebros.h"
 #include <math.h>
 
 /*-------- Conversions        ----------*/
@@ -46,10 +46,7 @@ vec3 linalgNormalizeVec3(vec3 a) {
 
 	vec3 result;
 
-	result.data[0] = a.data[0] * invMagnitude;
-	result.data[1] = a.data[1] * invMagnitude;
-	result.data[2] = a.data[2] * invMagnitude;
-	result.data[3] = 0.0f;
+	result.vector = _mm_mul_ps(a.vector, _mm_set1_ps(invMagnitude));
 
 	return result;
 }
@@ -81,32 +78,101 @@ vec3 linalgMulVec3(vec3 a, float scalar) {
 	return result;
 }
 
-float angleBetweenVectors3(vec3 a, vec3 b) {
+float linalgAngleBetweenVectors3(vec3 a, vec3 b) {
 
-	float magA = linalgDotVec3(a, a);
-	float magB = linalgDotVec3(b, b);
-	float denominator = magA * magB;
-
-	if (denominator < eps) {
-		return 0.0f;
-	}
+	float denominator = sqrtf(linalgDotVec3(a, a) * linalgDotVec3(b, b));
 
 	float dot = linalgDotVec3(a, b);
-	denominator = sqrtf(denominator);
 
 	return linalgRad2Deg(acosf(dot / denominator));
 }
 
-vec3 project(vec3 incoming, vec3 basis) {
-	
-	float scale = linalgDotVec3(incoming, basis) / linalgDotVec3(basis, basis);
+vec3 linalgProject(vec3 incoming, vec3 basis) {
 
-	return linalgMulVec3(basis, scale);
+	return linalgMulVec3(basis, linalgDotVec3(incoming, basis) / linalgDotVec3(basis, basis));
 }
 
-vec3 reject(vec3 incoming, vec3 basis) {
+vec3 linalgReject(vec3 incoming, vec3 basis) {
 	
-	return linalgSubVec3(incoming, project(incoming, basis));
+	return linalgSubVec3(incoming, linalgProject(incoming, basis));
+}
+
+vec3 linalgReflect(vec3 incident, vec3 normal) {
+
+	vec3 result;
+	// reflected = incident − 2(incident.normal)normal
+	result.vector = _mm_fmadd_ps(
+		normal.vector, 
+		_mm_set1_ps(-2.0f * linalgDotVec3(incident, normal)),
+		incident.vector
+	);
+
+	return result;
+}
+
+vec3 linalgLerpVec3(vec3 a, vec3 b, float t) {
+
+	vec3 result;
+
+	result.vector = _mm_fmadd_ps(
+		_mm_sub_ps(b.vector, a.vector),
+		_mm_set1_ps(t),
+		a.vector
+	);
+
+	return result;
+}
+
+vec3 linalgSlerpVec3(vec3 a, vec3 b, float t) {
+
+	if (t < 0.1f) {
+		return linalgLerpVec3(a, b, t);
+	}
+
+	float angle = linalgAngleBetweenVectors3(a, b);
+
+	float denominator = sinf(linalgDeg2Rad(angle));
+
+	vec3 result;
+
+	result.vector = _mm_fmadd_ps(
+		a.vector,
+		_mm_set1_ps(sinf(1 - t) * angle / denominator),
+		_mm_mul_ps(b.vector, _mm_set1_ps(sinf(t * angle) / denominator))
+	);
+
+	return result;
+}
+
+vec3 linalgNlerpVec3(vec3 a, vec3 b, float t) {
+
+	return linalgNormalizeVec3(linalgLerpVec3(a, b, t));
+}
+
+bool linalgCloseVec3(vec3 a, vec3 b) {
+	
+	vec3 displacement = linalgSubVec3(a, b);
+
+	return linalgDotVec3(displacement, displacement) < eps;
+}
+
+/*-------- Vector4 Operations ----------*/
+
+float linalgDotVec4(vec4 a, vec4 b) {
+	return a.data[0] * b.data[0] + a.data[1] * b.data[1] + a.data[2] * b.data[2] + a.data[3] * b.data[3];
+}
+
+vec4 linalgNormalizeVec4(vec4 a) {
+
+	float invMagnitude = 1.0f / sqrtf(
+		a.data[0] * a.data[0] + a.data[1] * a.data[1] + a.data[2] * a.data[2] + a.data[3] * a.data[3]
+	);
+
+	vec4 result;
+
+	result.vector = _mm_mul_ps(a.vector, _mm_set1_ps(invMagnitude));
+
+	return result;
 }
 
 /*-------- Matrix4 Operations ----------*/
@@ -271,4 +337,229 @@ mat4 linalgMulMat4Mat4(mat4 m1, mat4 m2) {
 	result.column_vector[3] = linalgMulMat4Vec4(m2, m1.column_vector[3]);
 
 	return result;
+}
+
+mat4 linalgAddMat4(mat4 m1, mat4 m2) {
+
+	mat4 m3;
+	m3.chunk[0] = _mm256_add_ps(m1.chunk[0], m2.chunk[0]);
+	m3.chunk[1] = _mm256_add_ps(m1.chunk[1], m2.chunk[1]);
+
+	return m3;
+}
+
+mat4 linalgMulMat4Scalar(mat4 matrix, float scalar) {
+
+	mat4 m3;
+	__m256 scale = _mm256_set1_ps(scalar);
+	m3.chunk[0] = _mm256_mul_ps(matrix.chunk[0], scale);
+	m3.chunk[1] = _mm256_mul_ps(matrix.chunk[1], scale);
+
+	return m3;
+}
+
+mat4 linalgLerpMat4(mat4 m1, mat4 m2, float t) {
+
+	mat4 m3;
+	__m256 scale = _mm256_set1_ps(t);
+
+	m3.chunk[0] = _mm256_fmadd_ps(
+		_mm256_sub_ps(m2.chunk[0], m1.chunk[0]),
+		scale,
+		m1.chunk[0]
+	);
+
+	m3.chunk[1] = _mm256_fmadd_ps(
+		_mm256_sub_ps(m2.chunk[1], m1.chunk[1]),
+		scale,
+		m1.chunk[1]
+	);
+
+	return m3;
+}
+
+mat4 linalgTranspose(mat4 matrix) {
+
+	mat4 transposed;
+
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			transposed.data[i + 4 * j] = matrix.data[j + 4 * i];
+		}
+	}
+
+	return transposed;
+}
+
+mat4 linalgTransformInverse(mat4 matrix) {
+
+	//Get the scale factors
+	float a = sqrtf(linalgDotVec4(matrix.column_vector[0], matrix.column_vector[0]));
+	float b = sqrtf(linalgDotVec4(matrix.column_vector[1], matrix.column_vector[1]));
+	float c = sqrtf(linalgDotVec4(matrix.column_vector[2], matrix.column_vector[2]));
+
+	//Get the rotation vectors, apply inverse scaling
+	vec4 X = linalgNormalizeVec4(matrix.column_vector[0]);
+	X.vector = _mm_mul_ps(X.vector, _mm_set1_ps(1 / a));
+	vec4 Y = linalgNormalizeVec4(matrix.column_vector[1]);
+	Y.vector = _mm_mul_ps(Y.vector, _mm_set1_ps(1 / b));
+	vec4 Z = linalgNormalizeVec4(matrix.column_vector[2]);
+	Z.vector = _mm_mul_ps(Z.vector, _mm_set1_ps(1 / c));
+	vec4 T = linalgNormalizeVec4(matrix.column_vector[3]);
+	T.vector = _mm_mul_ps(T.vector, _mm_set1_ps(-1));
+
+	mat4 inverse;
+
+	//Column adjustments
+	inverse.column[0] = _mm_setr_ps(X.data[0], Y.data[0], Z.data[0], 0);
+	inverse.column[1] = _mm_setr_ps(X.data[1], Y.data[1], Z.data[1], 0);
+	inverse.column[2] = _mm_setr_ps(X.data[2], Y.data[2], Z.data[2], 0);
+	inverse.column[3] = _mm_setr_ps(linalgDotVec4(X, T), linalgDotVec4(Y, T), linalgDotVec4(Z, T), 1);
+
+	return inverse;
+}
+
+/*-------- Quaternion Operations ----------*/
+
+quat linalgMakeQuaternionFromComponents(float x, float y, float z, float w) {
+	quat q;
+
+	q.vector = _mm_setr_ps(x, y, z, w);
+
+	return q;
+}
+
+quat linalgMakeQuaternionFromRotation(float angle, vec3 axis) {
+	quat q;
+
+	axis = linalgNormalizeVec3(axis);
+	float s = sinf(linalgDeg2Rad(angle / 2));
+	float c = cosf(linalgDeg2Rad(angle / 2));
+
+	q.vector = _mm_mul_ps(axis.vector, _mm_set1_ps(s));
+	q.data[3] = c;
+
+	return q;
+}
+
+quat linalgMakeRotationFromVec2Vec(vec3 a, vec3 b) {
+
+	a = linalgNormalizeVec3(a);
+	b = linalgNormalizeVec3(b);
+
+	// a and b might be antiparallel
+	if (linalgClose(a, linalgMulVec3(b, -1.0f))) {
+
+		//we want to rotate around a to get to b,
+		//pick the least dominant component as the rotation direction
+		vec3 ortho = linalgMakeVec3(1, 0, 0);
+		if (fabsf(a.data[1]) < fabs(a.data[0])) {
+			ortho = linalgMakeVec3(0, 1, 0);
+		}
+		if (fabsf(a.data[2]) < min(fabs(a.data[0]), fabs(a.data[1]))) {
+			ortho = linalgMakeVec3(0, 0, 1);
+		}
+		vec3 axis = linalgNormalizeVec3(linalgCross(a, ortho));
+		return linalgMakeQuaternionFromComponents(
+			axis.data[0], 
+			axis.data[1], 
+			axis.data[2], 
+			0.0f
+		);
+	}
+
+	//Construct the regular quaternion
+	vec3 halfVec = linalgNormalizeVec3(linalgAddVec3(a, b));
+	vec3 axis = linalgCross(a, halfVec);
+	return linalgMakeQuaternionFromComponents(
+		axis.data[0], 
+		axis.data[1], 
+		axis.data[2], 
+		linalgDotVec3(a, halfVec)
+	);
+}
+
+vec3 linalgGetAxisFromQuaternion(quat q) {
+	return linalgNormalizeVec3(linalgMakeVec3(q.data[0], q.data[1], q.data[2]));
+}
+
+float linalgGetAngleFromQuaternion(quat q) {
+	return linalgRad2Deg(2.0f * acosf(q.data[3]));
+}
+
+quat linalgAddQuat(quat q1, quat q2) {
+	quat result;
+
+	result.vector = _mm_add_ps(q1.vector, q2.vector);
+
+	return result;
+}
+
+quat linalgSubQuat(quat q1, quat q2) {
+	quat result;
+
+	result.vector = _mm_sub_ps(q1.vector, q2.vector);
+
+	return result;
+}
+
+quat linalgMulQuat(quat q, float scalar) {
+	quat result;
+
+	result.vector = _mm_mul_ps(q.vector, _mm_set1_ps(scalar));
+
+	return result;
+}
+
+float linalgDotQuat(quat q1, quat q2) {
+	return q1.data[0] * q2.data[0]
+		+ q1.data[1] * q2.data[1]
+		+ q1.data[2] * q2.data[2]
+		+ q1.data[3] * q2.data[3];
+}
+
+bool linalgCloseQuat(quat q1, quat q2) {
+
+	quat displacement = linalgSubQuat(q2, q1);
+
+	return linalgDotQuat(displacement, displacement) < eps;
+}
+
+bool linalgQuatSameOrientation(quat q1, quat q2) {
+
+	quat displacement = linalgSubQuat(q1, q2);
+
+	if (linalgDotQuat(displacement, displacement) < eps) {
+		return true;
+	}
+
+	displacement = linalgAddQuat(q1, q2);
+
+	if (linalgDotQuat(displacement, displacement) < eps) {
+		return true;
+	}
+
+	return false;
+}
+
+quat linalgNormalizeQuat(quat q) {
+
+	float scalar = 1 / sqrtf(linalgDotQuat(q, q));
+
+	return linalgMulQuat(q, scalar);
+}
+
+quat linalgGetConjQuat(quat q) {
+
+	return linalgMakeQuaternionFromComponents(
+		-q.data[0],
+		-q.data[1],
+		-q.data[2],
+		q.data[3]
+	);
+}
+
+quat linalgInvQuat(quat q) {
+
+	return linalgMulQuat(linalgGetConjQuat(q), 1 / linalgDotQuat(q, q));
 }
